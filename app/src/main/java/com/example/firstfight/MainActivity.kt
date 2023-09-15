d5df83a3ed420780d77bcd8d54b380b4ea9f31b6
lastbaks <lastbaks@gmail.com>
2023-09-15 13:15:42 +0300

    Fight Screen with icons

--- app/src/main/java/com/example/firstfight/MainActivity.kt
+++ app/src/main/java/com/example/firstfight/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firstfight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +20,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.firstfight.ui.theme.FirstFightTheme
@@ -60,12 +64,37 @@ fun FightWindow () {
             .fillMaxWidth()
             .background(Color.Green)
             .weight(12f)) {
-
+            EnemyPictureScreen()
+            PlayerPictureScreen()
         }
         Row(modifier = Modifier
             .fillMaxWidth()
             .background(Color.Blue)
             .weight(3f)) {
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .background(Color.Blue)
+                .weight(1f)) {
+                WeaponOne()
+            }
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .background(Color.White)
+                .weight(1f)) {
+                WeaponTwo()
+            }
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .background(Color.Gray)
+                .weight(1f)) {
+                WeaponThree()
+            }
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .background(Color.DarkGray)
+                .weight(1f)) {
+                UseWeapon()
+            }
         }
     }
 }
@@ -100,8 +129,54 @@ fun EnemyPictureScreen() {
     Box(modifier = Modifier
         .fillMaxHeight()
         .fillMaxWidth(0.5f)
-    ,
-    ){
+        .background(Color.DarkGray)
+        , contentAlignment = Alignment.Center ){
 
+    }
+}
+
+@Composable
+fun PlayerPictureScreen() {
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.LightGray)
+        , contentAlignment = Alignment.Center ){
+
+    }
+}
+
+@Composable
+fun WeaponOne(){
+    Button(modifier = Modifier.fillMaxSize()
+        , onClick = { /*TODO*/ }
+        , shape = RectangleShape) {
+        Image(painter = painterResource(id = R.drawable.hammer), contentDescription = "Hammer" )
+    }
+}
+
+@Composable
+fun WeaponTwo(){
+    Button(modifier = Modifier.fillMaxSize()
+        , onClick = { /*TODO*/ }
+        , shape = RectangleShape) {
+        Image(painter = painterResource(id = R.drawable.repire), contentDescription = "Hammer" )
+    }
+}
+
+@Composable
+fun WeaponThree(){
+    Button(modifier = Modifier.fillMaxSize()
+        , onClick = { /*TODO*/ }
+        , shape = RectangleShape) {
+        Image(painter = painterResource(id = R.drawable.axe), contentDescription = "Hammer" )
+    }
+}
+
+@Composable
+fun UseWeapon(){
+    Button(modifier = Modifier.fillMaxSize()
+        , onClick = { /*TODO*/ }
+        , shape = RectangleShape) {
+        Image(painter = painterResource(id = R.drawable.use), contentDescription = "Hammer" )
     }
 }
